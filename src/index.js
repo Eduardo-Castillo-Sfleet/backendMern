dd25c5e5c88ba145fe4ab28b33034eb464134d65
Eduardo-Castillo-Sfleet <eduardo.castillo@sfleet.mx>
2021-01-10 00:27:24 -0600

    port en settings

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ const app = require('./app')
 require('./database')
 
 async function main() {
-    await app.listen(4000);
-    console.log(`Puerto 4000 ON`);
+    await app.listen(app.get('port'));
+    console.log(`Puerto en ${app.get('port')}`);
 }
 
 main()
